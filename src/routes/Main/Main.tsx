97c0e0047f78cfa4e09ab69ea4926f6824207e1d
ChanLee <dlcks113@gmail.com>
2022-07-06 17:32:56 +0900

    CHORE: dev 코드 가져오기 위한 커밋

--- src/routes/Main/Main.tsx
+++ src/routes/Main/Main.tsx
@@ -1,5 +1,26 @@
 import React, { FC } from 'react';
+//import styled from 'styled-component';
 
+/*
+<MainWrapper>
+    <Header />
+
+    <PlanListWrapper>
+        PlanListTitle
+        PlanListContainer
+            LeftArrow
+                PlanListBox
+            RightArrow
+    <PlanListWrapper />
+
+    <LocationWrapper>
+        LocationTitle
+        LocationContainer
+    <LocationWrapper />
+
+<MainWrapper />
+
+*/
 const Main: FC = () => {
     return <div>Main</div>;
 };
