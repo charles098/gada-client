9e88cbdc51852ca8a857bc0b0c3d6f80ff43e22a
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-07-12 12:46:40 +0900

    CHORE: App.tsx 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ import { store } from 'store/config';
 
 // Pages
 import LoginHeader from 'components/LoginHeader';
-import Home from './routes/Home';
-import Login from './routes/Login';
-import LoginForm from './routes/LoginForm';
-import Register from './routes/Register'
-import Plan from './routes/Plan';
-import Main from './routes/Main';
+import Home from 'routes/Home';
+import Login from 'routes/Login';
+import LoginForm from 'routes/LoginForm';
+import Register from 'routes/Register';
+import Plan from 'routes/Plan';
+import Main from 'routes/Main';
 
 const App: FC = () => {
     return (
@@ -29,10 +29,10 @@ const App: FC = () => {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route element={<LoginHeader/>}>
-                            <Route path="/login" element={<Login />}/>
-                            <Route path="/login-form" element={<LoginForm />}/>
-                            <Route path="/register" element={ <Register /> }/>
+                        <Route element={<LoginHeader />}>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/login-form" element={<LoginForm />} />
+                            <Route path="/register" element={<Register />} />
                         </Route>
                         <Route path="/plan" element={<Plan />} />
                         <Route path="/main" element={<Main />} />
