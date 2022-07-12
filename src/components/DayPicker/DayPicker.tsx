8a12d96f5e1372f25ab290273eee52a75d761e63
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-07-12 14:56:21 +0900

    FEAT: slickslider 적용

--- src/components/DayPicker/DayPicker.tsx
+++ src/components/DayPicker/DayPicker.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import SlickSlider from 'components/SlickSlider';
+import { RootState } from 'store/modules';
 import styled from 'styled-components';
 
+const dayList = Array.from({ length: 8 }, (x, i) => i + 1);
+
+const startDateSelector = (state: RootState) => state.plan.startDate;
+const lastDateSelector = (state: RootState) => state.plan.lastDate;
+
 const DayPicker = () => {
-    const dayList = Array.from({ length: 3 }, (x, i) => i + 1);
+    const startDate = useSelector(startDateSelector);
+    const lastDate = useSelector(lastDateSelector);
+    const [planPeriod, setPlanPeriod] = useState<number | null>(null);
+
+    useEffect(() => {
+        setPlanPeriod(getPeriod(startDate, lastDate));
+    }, [startDate, lastDate]);
+
+    const getPeriod = (startDay: Date, lastDay: Date): number => {
+        const diffDate = startDay.getTime() - lastDay.getTime();
+        return Math.abs(diffDate / (1000 * 60 * 60 * 24));
+    };
 
     return (
         <Container>
-            <button type="button" className="all-button">
-                All
-            </button>
-            {dayList.map((day) => (
-                <button key={day} type="button" className="day-button">
-                    Day{day}
-                </button>
-            ))}
+            <SlickSlider
+                width={520}
+                speed={500}
+                slidesToShow={5}
+                slidesToScroll={2}
+                arrowPadding={0}
+                arrowSize={15}
+                itemCursor="default"
+            >
+                <ButtonCard>
+                    <Button type="button" className="selected-button">
+                        All
+                    </Button>
+                </ButtonCard>
+                {dayList.map((day) => (
+                    <ButtonCard>
+                        <Button key={day} type="button">
+                            Day{day}
+                        </Button>
+                    </ButtonCard>
+                ))}
+            </SlickSlider>
         </Container>
     );
 };
@@ -23,28 +56,28 @@ const Container = styled.div`
     height: 75px;
     display: flex;
     align-items: center;
+`;
 
-    & > .all-button {
-        cursor: pointer;
+const ButtonCard = styled.div`
+    width: 100px;
+    height: 35px;
+`;
+
+const Button = styled.button`
+    cursor: pointer;
+    width: 96px;
+    height: 100%;
+    border-radius: 20px;
+    border: solid 1px ${({ theme }) => theme.LIGHT_GRAY};
+    border-radius: 20px;
+    background-color: white;
+    font-size: 16px;
+
+    &.selected-button {
         border: solid 1px ${({ theme }) => theme.LIGHT_GRAY};
-        border-radius: 20px;
         background-color: ${({ theme }) => theme.PRIMARY};
-        color: white;
-        font-size: 17px;
-        width: 110px;
-        height: 40px;
-        margin-left: 15px;
-    }
-
-    & > .day-button {
-        cursor: pointer;
-        border: solid 1px ${({ theme }) => theme.LIGHT_GRAY};
-        border-radius: 20px;
-        background-color: white;
-        font-size: 17px;
-        width: 110px;
-        height: 40px;
-        margin-left: 15px;
+        color: #ffffff;
+        margin-right: 4px;
     }
 `;
 
