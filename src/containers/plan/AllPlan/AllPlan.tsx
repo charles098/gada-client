547c898eb231630582f9673d6248542e856e7c2d
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-07-15 14:24:20 +0900

    STYLE: allplan 레이아웃 완료

--- src/containers/plan/AllPlan/AllPlan.tsx
+++ src/containers/plan/AllPlan/AllPlan.tsx
@@ -9,7 +9,7 @@ interface IProps {
     planList: IPlace[][];
 }
 
-const testData = Array.from({ length: 10 });
+const testData = Array.from({ length: 5 });
 
 const AllPlan: FC<IProps> = ({ setIsAllPlan, planList }) => {
     // const onCloseAllPlan = () => {
@@ -22,8 +22,14 @@ const AllPlan: FC<IProps> = ({ setIsAllPlan, planList }) => {
                 close
             </button> */}
             {testData.map((data) => {
-                console.log('hi');
-                return <PlanCard>Route</PlanCard>;
+                return (
+                    <PlanCard>
+                        <DayContainer>
+                            <div className="day-num">Day 1</div>
+                            <div className="day-detail">7.12 Tue</div>
+                        </DayContainer>
+                    </PlanCard>
+                );
             })}
         </Container>
     );
@@ -33,10 +39,11 @@ const Container = styled.div`
     background-color: #eac4c4;
 
     width: 100%;
-    height: 620px;
+    min-height: 620px;
     margin: 0 30px 30px 30px;
     display: flex;
-    overflow: scroll;
+    /* flex-wrap: nowrap;  */
+    overflow-x: scroll;
 `;
 
 const PlanCard = styled.div`
@@ -44,7 +51,25 @@ const PlanCard = styled.div`
 
     width: 450px;
     margin: 0 30px 0px 30px;
-    display: block;
+    flex: 0 0 auto;
+`;
+
+const DayContainer = styled.div`
+    background-color: #ffffff;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: flex-end;
+
+    & > .day-num {
+        font-size: 20px;
+        margin: 10px;
+    }
+
+    & > .day-detail {
+        margin-bottom: 10px;
+        color: ${({ theme }) => theme.LIGHT_GRAY};
+    }
 `;
 
 export default AllPlan;
