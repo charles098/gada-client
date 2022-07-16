083d87aac3fef05de300f09ddf4ad4564037a9d5
ChanLee <dlcks113@gmail.com>
2022-07-17 02:16:39 +0900

    FIX: 스크롤바 밀림 버그 해결

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -5,5 +5,10 @@ export const GlobalStyle = createGlobalStyle`
   ${reset}
   body{
     font-family: 'Noto Sans KR', sans-serif;
+    overflow-y: scroll;
+  }
+
+  html>/**/body {
+    overflow-y: scroll;
   }
 `;
