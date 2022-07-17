a1be69cf2f9db7ba950e78962c7251aed933dad1
[SW2/서울/1반] 박상준 <lovehp30@gmail.com>
2022-07-17 11:41:54 +0900

    CHROE: Place Detail 관련 버튼 CSS 구성

--- src/containers/plan/PlanContents/EachPlan/EachPlanEditor/SetupRoute/RoutItem.tsx
+++ src/containers/plan/PlanContents/EachPlan/EachPlanEditor/SetupRoute/RoutItem.tsx
@@ -1,3 +1,4 @@
+import { ClockIcon, PencilIcon, WonIcon } from 'components/icons';
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
@@ -25,8 +26,24 @@ const PlaceBox: FC<IProps> = ({
             ref={focusRef}
             data-id={dataId}
         >
-            <Name>{placename}</Name>
-            <Location>{location}</Location>
+            <ItemsTexts>
+                <Name>{placename}</Name>
+                <Location>{location}</Location>
+            </ItemsTexts>
+            <ItemsToolBtn>
+                <div>
+                    <PencilIcon width={10} height={10} />
+                    <p>메모</p>
+                </div>
+                <div>
+                    <ClockIcon width={10} height={10} />
+                    <p>시간</p>
+                </div>
+                <div>
+                    <WonIcon width={10} height={10} />
+                    <p>비용</p>
+                </div>
+            </ItemsToolBtn>
         </Container>
     );
 };
@@ -44,11 +61,46 @@ const Container = styled.div`
     box-shadow: 1px 1px 10px 1px #d9d9d9;
     background-color: white;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: row;
+    contents-items: center;
 
     &.focus {
         background-color: ${({ theme }) => theme.PRIMARY_LIGHT};
+    }
+`;
+
+const ItemsTexts = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    contents-items: center;
+`;
+const ItemsToolBtn = styled.button`
+    text-decoration: none;
+    border: 0px;
+
+    display: flex;
+    background: none;
+    width: 60px;
+    padding-left: 10px;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: auto;
+    border-radius: 2px 13px 13px 2px;
+
+    transition: background 0.1s linear;
+
+    &:hover {
+        background: #dedede;
+        & > div > p {
+            color: #6d6d6d;
+        }
+    }
+
+    & > div > p {
+        display: inline;
+        font-size: 10px;
+        color: #9c9c9c;
     }
 `;
 
