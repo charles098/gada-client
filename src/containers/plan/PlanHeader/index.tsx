e714244a241f24895b7f6416985bc82f82fd6124
[SW2/서울/1반] 박상준 <lovehp30@gmail.com>
2022-07-18 17:57:29 +0900

    REFACOTR: PlanHeader 파일 구조 변경

--- src/containers/plan/PlanHeader/index.tsx
+++ src/containers/plan/PlanHeader/index.tsx
@@ -1,3 +1,57 @@
-import PlanInfo from './PlanInfo';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import PlanTitle from 'containers/plan/PlanHeader/PlanTitle';
+import PlanPeriod from 'components/PlanPeriod';
+import SwitchToggle from 'components/SwitchToggle';
+import { useDispatch } from 'react-redux';
+import { changeShareMode } from 'store/modules/plan/plan';
+
+const PlanInfo: FC = () => {
+    const dispatch = useDispatch();
+
+    const onClickSwitch = () => dispatch(changeShareMode());
+
+    return (
+        <Container>
+            <PlanTitle />
+            <PlanPeriod />
+            <PlanSwitch>
+                <p>공유하기</p>
+                <SwitchToggle
+                    label="Switch One"
+                    noText
+                    onClick={onClickSwitch}
+                />
+            </PlanSwitch>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    width: auto;
+    height: 30px;
+    margin: 30px;
+    display: flex;
+    flex-direction: row;
+
+    align-items: center;
+
+    & > button {
+        cursor: pointer;
+        border: none;
+        border-radius: 20px;
+        margin-top: 7px;
+        background-color: ${({ theme }) => theme.PRIMARY};
+        color: white;
+    }
+`;
+
+const PlanSwitch = styled.button`
+    padding: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-contents: center;
+    margin-left: auto;
+`;
 
 export default PlanInfo;
