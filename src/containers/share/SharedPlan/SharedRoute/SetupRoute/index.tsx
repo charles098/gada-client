d951f18899a6439bf9dcb207e5407d28a5e82e1d
[SW2/서울/1반] 박상준 <lovehp30@gmail.com>
2022-07-22 08:59:16 +0900

    CHORE: share container 생성

---
+++ src/containers/share/SharedPlan/SharedRoute/SetupRoute/index.tsx
@@ -0,0 +1,121 @@
+import React, {
+    FC,
+    useEffect,
+    useRef,
+    useCallback,
+    useState,
+    useMemo,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { ReactSortable } from 'react-sortablejs';
+import { RootState } from 'store/modules';
+
+import { sortSharedPlanList } from 'store/modules/plan/share';
+
+import { Place } from 'store/modules/plan';
+import { PlanDetailModel } from 'store/modules/plan/plan.model';
+import { useLocation } from 'react-router-dom';
+import getAuthHeader from 'utils/getAuthHeader';
+import RoutItem from './RoutItem';
+
+const planListSelector = (state: RootState) => state.share.planList;
+
+const setDaySelector = (state: RootState) => state.share.setDay;
+// eslint-disable-next-line no-underscore-dangle
+const planIdSelector = (state: RootState) => state.share._id;
+const grabPlaceOptionIdSelector = (state: RootState) =>
+    state.plan.grabPlaceOptionId;
+
+const SetupRoute: FC = () => {
+    const dispatch = useDispatch<any>();
+    const planList = useSelector(planListSelector);
+    const setDay = useSelector(setDaySelector);
+    const planId = useSelector(planIdSelector);
+    const location = useLocation();
+
+    const droppedRef = useRef<HTMLElement | null>(null);
+    const enterCnt = useRef(0);
+
+    // SortableJs Logic
+    type PlanDetailSortableItem = PlanDetailModel & { chosen: boolean };
+    const getSortableList = (
+        list: PlanDetailModel[][],
+    ): PlanDetailSortableItem[] => {
+        if (!(list.length > 1)) return [];
+        return list[setDay].map((x) => {
+            return {
+                ...x,
+                chosen: true,
+            };
+        });
+    };
+
+    const SortableList = useMemo(() => {
+        const temp = getSortableList(planList);
+        if (location) {
+            return temp;
+        }
+        return temp;
+    }, [planList, setDay]);
+
+    const onSort = (list: PlanDetailModel[]): void => {
+        if (!(list.length > 0)) return;
+        dispatch(sortSharedPlanList(list));
+    };
+
+    return (
+        <Container onDragOver={(e) => e.preventDefault()}>
+            {SortableList && (
+                <ReactSortable
+                    animation={150}
+                    list={SortableList}
+                    setList={onSort}
+                >
+                    {planList.length > 1 &&
+                        planList[setDay].map((plan: Place, index: number) => {
+                            return (
+                                <RoutItem
+                                    focusRef={
+                                        index === planList[setDay].length - 1
+                                            ? (droppedRef as React.RefObject<HTMLDivElement>)
+                                            : null
+                                    }
+                                    key={plan.id}
+                                    dataId={plan.id}
+                                    placename={plan.name}
+                                    location={plan.address}
+                                />
+                            );
+                        })}
+                </ReactSortable>
+            )}
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    width: 450px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 15px;
+    position: relative;
+    box-sizing: border-box;
+
+    &.drag-over {
+        width: 450px;
+        border: solid 2px ${({ theme }) => theme.PRIMARY};
+        border-radius: 20px;
+        background-color: ${({ theme }) => theme.PRIMARY_LIGHT};
+    }
+
+    &.drag-over {
+        border: solid 2px ${({ theme }) => theme.PRIMARY};
+        border-radius: 20px;
+        background-color: ${({ theme }) => theme.PRIMARY_LIGHT};
+    }
+`;
+
+export default SetupRoute;
