b07a327e1a29a56a87a45f56eee364c7ac645eec
ChanLee <dlcks113@gmail.com>
2022-07-25 20:42:08 +0900

    CHORE: 필요 없는 코드 제거

--- src/routes/Share.tsx
+++ src/routes/Share.tsx
@@ -68,38 +68,6 @@ const Share = () => {
 
 export default Share;
 
-const ShareHeader = styled.section`
-    min-width: 1287px;
-    width: 100%;
-    background-color: #e4f0ff;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    padding: 60px 0;
-`;
-const TitleContainer = styled.div`
-    width: 1287px;
-    margin: 0 auto;
-    color: #333;
-`;
-const MainTitle = styled.h1`
-    font-size: 30px;
-    font-weight: bold;
-    letter-spacing: 1px;
-`;
-
-const Subtitle = styled.h2`
-    font-size: 22px;
-    font-weight: 400;
-    letter-spacing: 1.5px;
-`;
-const SubTitle1 = styled(Subtitle)`
-    margin-top: 35px;
-    margin-bottom: 15px;
-`;
-const SubTitle2 = styled(Subtitle)``;
-
 const ShareBody = styled.main`
     min-width: 1287px;
     width: 100%;
